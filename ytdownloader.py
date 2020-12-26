475e91f9efdfd9ba8873a0abdaa96249f5ed593f
SagarR.B <sagarrb1805@gmail.com>
2020-12-26 19:06:10 +0530

    added the function of button and tested a single sample

--- ytdownloader.py
+++ ytdownloader.py
@@ -6,11 +6,21 @@ root.geometry("500x500")
 root.title("Youtube Downloader")
 root.config(bg="black")
 
-youtubelink = StringVar()
-linkentry_space = Entry(root, textvar=youtubelink, width=60)
-linkentry_space.place(x=30, y=250)
 
-down_but = Button(root, text="download", bg="yellow", fg="brown")
+def download():
+    url = youtube_link.get()
+
+    youtube = pytube.YouTube(url)
+
+    video = youtube.streams.first()
+    video.download()
+
+
+youtube_link = StringVar()
+link_entry_space = Entry(root, textvar=youtube_link, width=60)
+link_entry_space.place(x=30, y=250)
+
+down_but = Button(root, text="download", bg="yellow", fg="brown", command=download)
 down_but.place(x=410, y=246)
 
 
