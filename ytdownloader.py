f5b07f2e9ee7bd2641f2cacc3517f9a82fd43182
SagarR.B <sagarrb1805@gmail.com>
2020-12-26 19:11:40 +0530

    added the invalid lik condition

--- ytdownloader.py
+++ ytdownloader.py
@@ -9,11 +9,14 @@ root.config(bg="black")
 
 def download():
     url = youtube_link.get()
+    try:
+        youtube = pytube.YouTube(url)
+        video = youtube.streams.first()
+        video.download()
+    except:
+        error_msg = Label(root, text="invalid link", width=20, font=("arial", 10, "bold"))
+        error_msg.place(x=150, y=30)
 
-    youtube = pytube.YouTube(url)
-
-    video = youtube.streams.first()
-    video.download()
 
 
 youtube_link = StringVar()
@@ -22,6 +25,5 @@ link_entry_space.place(x=30, y=250)
 
 down_but = Button(root, text="download", bg="yellow", fg="brown", command=download)
 down_but.place(x=410, y=246)
-
 
 root.mainloop()
